c1c9a8f4b322f683af8b8aa0f7ba9a116db201ff
Dawid Paleczny <dawidpaleczny27@gmail.com>
2023-07-17 20:28:27 +0200

    productService

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,49 +1,9 @@
 public sealed class Product permits ProductService {
-    private int productId;
-    private double price;
-    private String name;
-    private Category category;
-    private int quantity;
-
-    public Product(int productId, double price, String name, Category category, int quantity) {
-        this.productId = productId;
-        this.price = price;
-        this.name = name;
-        this.category = category;
-        this.quantity = quantity;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
+    protected int productId = 1;
+    protected double price;
+    protected String name;
+    protected Category category;
+    protected int quantity;
 
     protected boolean validatePrice(double price) {
         return price > 0;
--- src/main/java/ProductService.java
+++ src/main/java/ProductService.java
@@ -1,2 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public final class ProductService extends Product {
+    Set<Product> productSet = new HashSet<Product>();
+    public void addProduct(double price, String name, Category category, int quantity) {
+        super.productId++;
+        this.price = price;
+        this.name = name;
+        this.category = category;
+        this.quantity = quantity;
+        productSet.add(this);
+    }
+
+
 }
