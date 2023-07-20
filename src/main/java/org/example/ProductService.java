aa12c837675d62f1ede71ab5b1512c478e74fbd3
Dawid Paleczny <dawidpaleczny27@gmail.com>
2023-07-20 19:44:02 +0200

    Menu3

--- src/main/java/org/example/ProductService.java
+++ src/main/java/org/example/ProductService.java
@@ -3,13 +3,9 @@ package org.example;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ProductService extends Product {
+public class ProductService {
     private List<Product> products;
     private int nextProductId = 1;
-    public ProductService(int productId, double price, String name, Category category, int quantity, List<Product> products) {
-        super(productId, price, name, category, quantity);
-        this.products = products;
-    }
 
     public void addProduct(double price, String name, Category category, int quantity) {
         Product product = new Product(nextProductId, price, name, category, quantity);
