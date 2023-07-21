b1f0ab7dc65e9319f5a70498214246bc5612c24a
Karolina Zalasińska <karolinaa.zalasinska@gmail.com>
2023-07-21 21:51:56 +0200

    Created models: Product Category Order ProductService Order Status Category Service Order Service Main

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -11,22 +11,14 @@ public class ProductService {
     private List<Product> products = new ArrayList<>();
     private int lastUsedProductId;
 
-    public List<Product> createProducts(List<Category> categories) {
-        List<Product> products = new ArrayList<>();
-
-        products.add(new Product(generateProductId(), 199.99, "Dress", categories.get(0)));
-        products.add(new Product(generateProductId(), 149.99, "Heels", categories.get(1)));
-        products.add(new Product(generateProductId(), 39.99, "Cap", categories.get(2)));
-        products.add(new Product(generateProductId(), 79.99, "Trousers", categories.get(0)));
-        products.add(new Product(generateProductId(), 249.99, "Sneakers", categories.get(1)));
-        products.add(new Product(generateProductId(), 19.99, "Hat", categories.get(2)));
-        return products;
-    }
-
     private int generateProductId() {
         return lastUsedProductId++;
     }
-    
+
+    public Product createProduct(double price, String name, Category category) {
+        return new Product(generateProductId(), price, name, category);
+    }
+
     public void addProduct(Product product) {
         products.add(product);
     }
